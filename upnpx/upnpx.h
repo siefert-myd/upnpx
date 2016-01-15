424ed0c13ca2a56a74b195bd39dd762befafee2c
Sean Siefert <ssiefert@mydevices.com>
2016-01-15 10:42:34 -0800

    try again

---
+++ upnpx/upnpx.h
@@ -0,0 +1,20 @@
+//
+//  upnpx.h
+//  upnpx
+//
+//  Created by Sean Siefert on 1/14/16.
+//  Copyright © 2016 Bruno Keymolen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for upnpx.
+FOUNDATION_EXPORT double upnpxVersionNumber;
+
+//! Project version string for upnpx.
+FOUNDATION_EXPORT const unsigned char upnpxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <upnpx/PublicHeader.h>
+
+#import "UPnPDB.h"
+#import "UPnPManager.h"
